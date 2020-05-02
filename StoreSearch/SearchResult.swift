eab6a269b3f11794df2d05ed28a45cf8e51a0b09
Buck <buck@buckrozelle.com>
2020-05-02 11:13:08 -0400

    Add search bar and table view.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Buck Rozelle on 5/1/20.
+//  Copyright © 2020 buckrozelledotcomLLC. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
